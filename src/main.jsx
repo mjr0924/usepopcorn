9526bed181332795ae8cf3604a8ab214a5b39e2e
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-03-10 03:32:34 +0330

    add propTypes config to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import StarRating from "./StarRaiting";
+import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App.jsx";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRaiting={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+    <Test />
   </StrictMode>
 );
