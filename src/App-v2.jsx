a336308b05f702e336fe9b187c02da612fe9ef2a
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-05-15 17:49:17 +0330

    debug fetch from omdbapi

--- src/App-v2.jsx
+++ src/App-v2.jsx
@@ -80,7 +80,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) {
@@ -355,7 +355,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       setIsLoading(true);
       async function getMovieDetails() {
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
